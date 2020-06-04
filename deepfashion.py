5f43186c995848dccf47c25c53c91b5fb0786c93
ssoyyoung <ssoyyoung.p@gmail.com>
2020-06-04 18:34:21 +0900

    first commit for image retrieval

---
+++ deepfashion.py
@@ -0,0 +1,43 @@
+import struct
+import glob
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import tensorflow.keras.layers as layers
+from tensorflow.keras.models import Model
+
+def preprocess(img_path, input_shape):
+    img = tf.io.read_file(img_path)
+    img = tf.image.decode_jpeg(img, channels=input_shape[2])
+    img = tf.image.resize(img, input_shape[:2])
+    img = preprocess_input(img)
+    return img
+    
+
+def main():
+    batch_size = 100
+    input_shape = (224, 224, 3)
+    base = tf.keras.applications.MobileNetV2(input_shape=input_shape,
+                                             include_top=False,
+                                             weights='imagenet')
+    base.trainable = False
+
+    model = Model(inputs=base.input, outputs=layers.GlobalAveragePooling2D()(base.output))
+
+    fnames = glob.glob('/data/deepfashion/images/*/*.jpg', recursive=True)
+    list_ds = tf.data.Dataset.from_tensor_slices(fnames)
+    ds = list_ds.map(lambda x: preprocess(x, input_shape), num_parallel_calls=-1)
+    dataset = ds.batch(batch_size).prefetch(-1)
+
+    with open('data/deepfashion/fvecs.bin', 'wb') as f:
+        for batch in dataset:
+            fvecs = model.predict(batch)
+
+            fmt = f'{np.prod(fvecs.shape)}f'
+            f.write(struct.pack(fmt, *(fvecs.flatten())))
+
+    with open('data/deepfashion/fnames.txt', 'w') as f:
+        f.write('\n'.join(fnames))
+
+if __name__ == '__main__':
+    main()
